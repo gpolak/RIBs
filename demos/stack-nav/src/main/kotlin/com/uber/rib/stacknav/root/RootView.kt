6d8347aa9d86b2f26b609b30abdbb5e14f58a8e5
George Polak <georgep@uber.com>
2026-05-23 18:27:32 -0400

    demo

---
+++ demos/stack-nav/src/main/kotlin/com/uber/rib/stacknav/root/RootView.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2025. Uber Technologies
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.uber.rib.stacknav.root
+
+import android.content.Context
+import android.widget.FrameLayout
+
+/** Container that holds whichever screen is currently on top of the stack. */
+class RootView(context: Context) : FrameLayout(context)
